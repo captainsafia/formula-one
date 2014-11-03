031cabfab95c03e9305cb895bc2fd0c6ae0a329d
Safia Abdalla <seabdalla@gmail.com>
2014-11-03 14:10:07 -0600

    Updated users route with view render

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Resume = require("../models/resume");
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+/* GET user listing. */
+router.get('/:id', function(req, res) {
+  var id = req.params.id;
+  Resume.findOne({_id: id}).populate('user').exec(function(error, resume) {
+    res.render('user', {
+      person: resume
+    });
+  })
 });
 
 module.exports = router;

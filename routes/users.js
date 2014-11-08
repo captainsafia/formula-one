6ca9dd71542cf1495dde10ec3a9867467ba446d0
Safia Abdalla <seabdalla@gmail.com>
2014-11-07 18:14:27 -0600

    Added GET and POST routing for new user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Resume = require("../models/resume");
+var User = require("../models/user");
 
 /* GET user listing. */
 router.get('/:id', function(req, res) {
@@ -10,6 +11,27 @@ router.get('/:id', function(req, res) {
       person: resume
     });
   })
+});
+
+/* POST new user */
+router.post('/new', function(req, res) {
+  var data = req.body;
+  User.create(data, function(error, user) {
+    if (error)  {
+      console.log(error);
+      // Send to 500 page
+    } 
+    Resume.create({"user" : user._id, "filepicker" : filepicker_url}, function(error, resume) {
+      if (error)  {
+        console.log(error);
+      }
+        // Redirect to success page 
+      });
+  });
+});
+
+router.get('/new', function(req, res) {
+  res.render('new');
 });
 
 module.exports = router;

1f6bb18bc37765dcf23ea07f28c75af7ff1600d9
Safia Abdalla <seabdalla@gmail.com>
2014-11-07 20:01:34 -0600

    Updated resume search routes

--- routes/search.js
+++ routes/search.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 var Resume = require("../models/resume");
 
-router.get('/:key', function(req, res) {
-  var key = req.params.key;
-  if (valid(key)) {
-    console.log('Search page!');
-  } else {
-    console.log('Auth error page!');
-  }
+router.get('/', function(req, res) {
+  res.render('search');
 });
+
+router.get('/:query', function(req, res) {
+  console.log(req.params.query);
+});
+
+module.exports = router;

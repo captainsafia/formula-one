4e2259d8c84ebb4401b06aafad214ad63aee371f
Safia Abdalla <seabdalla@gmail.com>
2014-11-15 16:48:28 -0600

    Added initial version of fillable resume model

--- models/resume.js
+++ models/resume.js
@@ -5,6 +5,30 @@ var schema = mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   },
+  work: [{
+    company : String,
+    position: String,
+    startDate: Date,
+    endDate: Date,
+    summary: String
+  }],
+  education: [{
+    institution: String,
+    major: String,
+    startDate: Date,
+    endDate: Date,
+    GPA: Number
+  }],
+  skills: [{
+    skill: String,
+    level: Number
+  }],
+  title: [{
+    title: String, 
+    date: String, 
+    awarder: String, 
+    summary: String
+  }],
   filepicker: {
     type: String,
     trim: true

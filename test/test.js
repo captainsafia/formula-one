1ab02ebe5ea941c6dfbf52a535dc6a66a8cde7ee
Safia Abdalla <seabdalla@gmail.com>
2014-12-23 22:05:35 -0600

    Updated test model creation

--- test/test.js
+++ test/test.js
@@ -7,61 +7,68 @@ mockgoose(mongoose);
 var Resume = require("../models/resume");
 var User = require("../models/user");
 var searchByMajor = require("../routes/search").searchByMajor;
+var searchByName = require("../routes/search").searchByName;
 
 beforeEach(function(done) {
 	mockgoose.reset();
-	var john = User.create({
-		name: "John Doe",
-		email: "johndoe@email.com",
-		year: "Freshman",
-		university: "Northwestern University",
-		major: "Computer Science",
-		international: false,
-		epic: false,
-	});
-	Resume.create({
-		user: john._id, 
-		education: [{
-			institution: "Northwestern University",
-			major: "Computer Science",
-			year: "Freshman",
-			startDate : new Date(2014, 09, 25),
-			endDate: new Date(2018, 06, 26),
-			GPA: 3.96
-		}]
-	}, function(error, user) {
-		if (error) {
-			throw error;
-		}
-		done();
-	});
+		var resume = new Resume({
+			education: [{
+				institution: "Northwestern University",
+				major: "Computer Science",
+				year: "Freshman",
+				startDate : new Date(2014, 09, 25),
+				endDate: new Date(2018, 06, 26),
+				GPA: 3.96
+			}]
+		});
+		resume.save(function(error, resume) {
+			if (error) {
+				throw error;
+			}
+    var john = new User({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      year: "Freshman",
+      university: "Northwestern University",
+      major: "Computer Science",
+      international: false,
+      epic: false,
+      resume: resume._id,
+      });
+    john.save(function(error, user) {
+      if (error) {
+        throw error; 
+      }
+      done();
+    })
+  });
 });
 
-describe("Resume", function() {
-	describe("#searchByMajor", function() {
-		it("should return an empty array if nothing is found", function(done) {
-			searchByMajor("Clowning", function(resume) {
-				expect(resume).to.be.empty;
-				done();
-			})
-		});
-		it("should return a array if something is found", function (done) {
-			searchByMajor("Computer Science", function(resume) {
-				expect(resume).to.not.be.empty;
-				done();
-			})
-		})
-	});
-	describe("#searchByName", function() {
-		it("should return an empty array if nothing is found", function(done) {
-			searchByName("Steve Smith", function(resume) {
-				expect(resume).to.be.empty;
-				done();
-			});
-		});
-		it("should return an array if something is found", function(done) {
-			searchByName("John Doe", function(resume) {
-				expect(resume).to.not.be.empty;
+  describe("Resume", function() {
+    describe("#searchByMajor", function() {
+      it("should return an empty array if nothing is found", function(done) {
+        searchByMajor("Clowning", function(resume) {
+          expect(resume).to.be.empty;
+          done();
+        })
+      });
+      it("should return a array if something is found", function (done) {
+        searchByMajor("Computer Science", function(resume) {
+          expect(resume).to.not.be.empty;
+          done();
+        })
+      })
+    });
+    describe("#searchByName", function() {
+      it("should return an empty array if nothing is found", function(done) {
+        searchByName("Steve Smith", function(resume) {
+          expect(resume).to.be.empty;
+          done();
+        });
+      });
+      it("should return an array if something is found", function(done) {
+        searchByName("John Doe", function(resume) {
+          expect(resume).to.not.be.empty;
 				done();
 			});
 		});
